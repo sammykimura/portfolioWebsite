aa05a5da71c536e6b2e45cded1c31830b7f8a9ef
sammykimura <91994204+sammykimura@users.noreply.github.com>
2021-11-09 16:48:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let mushrooms=document.getElementById('mushrooms');
-
+let imgWrap= document.getElementsByClassName('imgWrap')
 
 function expandPic(){
   mushrooms.style.width="500px";
@@ -7,3 +7,19 @@ function expandPic(){
 }
 
 mushrooms.addEventListener('mouseenter',expandPic());
+
+
+function shrinkPic(){
+  mushrooms.style.width="90%";
+  console.log("width changed back");
+}
+
+
+mushrooms.addEventListener('mouseout',shrinkPic());
+
+
+function expandFrame(){
+  imgWrap.style.width="500px";
+  console.log("frame expanded")
+}
+imgWrap.addEventListener('mouseenter',expandFrame());
